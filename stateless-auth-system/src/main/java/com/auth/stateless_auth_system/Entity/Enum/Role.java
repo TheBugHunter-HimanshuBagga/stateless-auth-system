812559301de950769442b37e094e7c5af9bb0967
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-02-28 12:10:28 +0530

    UserEntityAdded

---
+++ stateless-auth-system/src/main/java/com/auth/stateless_auth_system/Entity/Enum/Role.java
@@ -0,0 +1,5 @@
+package com.auth.stateless_auth_system.Entity.Enum;
+
+public enum Role {
+    ADMIN , USER
+}
---
+++ stateless-auth-system/src/main/java/com/auth/stateless_auth_system/Entity/User.java
@@ -0,0 +1,75 @@
+package com.auth.stateless_auth_system.Entity;
+
+import com.auth.stateless_auth_system.Entity.Enum.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+/*
+
+UserDetails allows Spring to:
+
+ Read stored password
+ Identify user
+ Understand roles
+ Authenticate login
+ Authorize access
+
+ */
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+    // Whether the user is allowed to authenticate or not.
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
