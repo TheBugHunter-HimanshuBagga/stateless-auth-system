e83ae889d1454bc4758d6ce27a33aa17873cd68f
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-02-28 11:57:01 +0530

    Basic-SETUP

---
+++ stateless-auth-system/src/main/java/com/auth/stateless_auth_system/StatelessAuthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.auth.stateless_auth_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatelessAuthSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatelessAuthSystemApplication.class, args);
+	}
+
+}
---
+++ stateless-auth-system/src/test/java/com/auth/stateless_auth_system/StatelessAuthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.stateless_auth_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatelessAuthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
