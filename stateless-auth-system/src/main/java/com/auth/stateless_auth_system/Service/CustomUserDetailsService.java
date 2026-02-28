cea24822ffd01bc211ce42b8b172c9ca126d7683
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-02-28 12:28:08 +0530

    CustomUserDetailsService

---
+++ stateless-auth-system/src/main/java/com/auth/stateless_auth_system/Service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.auth.stateless_auth_system.Service;
+
+import com.auth.stateless_auth_system.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    }
+}
+
