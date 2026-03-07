1f38643f5818e4e5f0fc03e9ac1f1db0d90815c5
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-03-07 14:36:01 +0530

    Added'SecurityFilterChain'

---
+++ stateless-auth-system/src/main/java/com/auth/stateless_auth_system/Config/SecurityConfig.java
@@ -0,0 +1,89 @@
+package com.auth.stateless_auth_system.Config;
+
+import com.auth.stateless_auth_system.Service.CustomUserDetailsService;
+import com.auth.stateless_auth_system.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    private final UserDetailsService userDetailsService;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) //Every request must bring its own JWT
+                )
+                .authenticationProvider(authenticationProvider())//You are registering it inside Spring Security.
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
+
+/*
+
+/auth/login
+/auth/register
+
+are public APIs.
+
+Everything else:
+
+/users
+/products
+/orders
+
+requires authentication.
+
+ */
+
+/*
+DaoAuthenticationProvider is a Spring Security authentication provider that authenticates users using data from a database (DAO layer).
+
+DAO = Data Access Object
+
+So it means:
+
+User login
+   ↓
+DaoAuthenticationProvider
+   ↓
+Fetch user from database
+   ↓
+Check password
+   ↓
+Authenticate user
+ */
